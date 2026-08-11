c506a4690f5791bf60a4bab6336b224d4da86153
VINOTHINI <vinothini2007periyasamy@gmail.com>
2026-08-11 21:46:01 +0530

    Add files via upload

---
+++ clgdetails.java
@@ -0,0 +1,12 @@
+public class clgdetails {
+    
+    public static void main(String[] args) {
+        System.out.println("College Name: PPG Institute of Technology");
+        System.out.println("Department: Information Technology");
+        System.out.println("City: Coimbatore");
+    }
+}
+    
+
+
+
---
+++ today.java
@@ -0,0 +1,6 @@
+public class today {
+    
+    public static void main(String[] args) {
+        System.out.println("Today's Date: " + System.currentTimeMillis());
+    }
+}
